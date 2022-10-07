27eed1a43991e055edeb91502bfca5adf2cd603a
Niklas Bergen <84533378+NB094@users.noreply.github.com>
2022-10-06 23:07:33 -0600

    Upload question 136 solution

---
+++ Q136-Single-Number.cs
@@ -0,0 +1,21 @@
+﻿// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+int[] nums = {5, 7, 6, 5, 7};
+
+
+var result = nums.GroupBy(x => x).Select(x => new {Number = x.Key, Count = x.Count()});
+
+foreach (var x in result)
+{
+        if (x.Count == 1)
+        {
+            Console.WriteLine($"This number only appears once: {x.Number}");
+            // return x.Number;
+        }
+}
+
+if (nums.Length == 1)
+    Console.WriteLine($"The array's length is 1, so the final answer is: {nums[0]}");
+// return nums[0];
